b4321f81261df36cbf4197fd27acd19e563cca0e
robertradulescu <robertvalentin11022000@gmail.com>
2023-02-18 14:59:55 +0200

    Functionalitate autocomplete

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using LicentaFinal.Models;
+﻿using LicentaFinal.Data;
+using LicentaFinal.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient.DataClassification;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace LicentaFinal.Controllers
@@ -7,10 +10,49 @@ namespace LicentaFinal.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private ApplicationDbContext Context { get; }
 
-        public HomeController(ILogger<HomeController> logger)
+
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context )
         {
             _logger = logger;
+            Context = context;
+        }
+
+        [HttpPost]
+        public JsonResult AutoComplete(string prefix) 
+        {
+            var stocuri= (from Stocuri in Context.Stocuri
+                          
+                          where Stocuri.NumeProdus.StartsWith(prefix)
+                          select new
+                          {
+                              label= Stocuri.NumeProdus,
+                              val= Stocuri.Id
+
+                          }).ToList();
+                          
+            return Json(stocuri); 
+        }
+
+        [HttpGet]
+        public IActionResult AutocompleteNumeProdus(string term)
+        {
+            var results = Context.Stocuri
+                            .Where(s => s.NumeProdus.Contains(term))
+                            .Select(s => s.NumeProdus)
+                            .Take(10)
+                            .ToList();
+            return Json(results);
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(string NumeProdus, string ProdusId)
+        {
+            ViewBag.Message = "Nume Produs: " + NumeProdus + " Id Produs: " + ProdusId;
+            return View();
         }
 
         public IActionResult Index()
